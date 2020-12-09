c18be46c6a4aef5f470e1b4e7aacc53315173930
Reza <hr.ferdowsi@gmail.com>
2020-12-09 22:05:53 +1100

    Reza- Removing Navbar

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/layout.module.css';
+
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import Navbar from '../components/Navbar';
