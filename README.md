0d82c1ba80253a88729ee69b307ecf4c2462c893
Reza <hr.ferdowsi@gmail.com>
2020-12-09 17:47:13 +1100

    Reza-3rd Commit-fixing the template

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-This is a starter template for [Learn Next.js](https://nextjs.org/learn).# pomodoro
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
 
 
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import Head from 'next/head';
+import styles from '../styles/layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
 const name: string = 'Reza'
 export const siteTitle: string = 'Next.js Sample Website'
